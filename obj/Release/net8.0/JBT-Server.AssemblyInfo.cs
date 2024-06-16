62e58aa26434b1d4691ea732e9763154fa819230
Brian Hockenjos <67165421+Bigimus@users.noreply.github.com>
2024-06-16 18:02:45 -0400

    update

--- obj/Release/net8.0/JBT-Server.AssemblyInfo.cs
+++ obj/Release/net8.0/JBT-Server.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("JBT-Server")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d549fe319cbb3eea81d120f048155f162b7697")]
 [assembly: System.Reflection.AssemblyProductAttribute("JBT-Server")]
